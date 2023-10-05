f528696f9198b50f3ecaac9e7c67923d01fde411
sitthisak <sitthisakbannob@gmail.com>
2023-10-06 01:10:45 +0700

    form for input name and image for create server

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -56,7 +56,41 @@ export const InitialModal = () =>{
                         Give your server a personality with a name and an miage. You can always change it later.
                     </DialogDescription>
                 </DialogHeader>
-
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="space-y-8 px-6">
+                            <div className="flex item-center justify-center text-center">
+                                TODO: Image Upload
+                            </div>
+                            <FormField 
+                                control={form.control}
+                                name="name"
+                                render={({field})=>(
+                                    <FormItem>
+                                        <FormLabel
+                                            className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                                        >
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input 
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="bg-gray-1000 px-6 py-4">
+                            <Button disabled={isLoading}>
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     )
