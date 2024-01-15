d8b2a09d6a1cf9c2e9aa7a5603fa5ae0cb694310
sitthisak <sitthisakbannob@gmail.com>
2024-01-15 22:33:03 +0700

    upload form and config

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -8,6 +8,7 @@ import {
     DialogTitle
 } from "@/components/ui/dialog"
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -20,7 +21,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1,{
@@ -71,7 +72,21 @@ export const InitialModal = () =>{
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex item-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload 
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField 
                                 control={form.control}
