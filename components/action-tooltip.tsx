fd2641c0577eeccb0cb0b141c6c9876b777ff2f7
sitthisak <sitthisakbannob@gmail.com>
2024-03-17 16:08:04 +0700

    add side bar and action(add servers) buttom

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+interface ActionTooltipProp {
+    label: string;
+    children: React.ReactNode;
+    side?: "top" | "right" | "bottom" | "left";
+    align?: "start" | "center" | "end";
+}
+
+export const ActionTooltop = ({
+    label,
+    children,
+    side,
+    align
+}:ActionTooltipProp) => {
+    return(
+        <TooltipProvider>
+            <Tooltip delayDuration={50}>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent side={side} align={align}>
+                    {label.toLowerCase()}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+}
